cd4f93a6e46c7ec522f8c0463e5d823a49979605
Artem <167626602+Artem7521@users.noreply.github.com>
2024-05-04 22:54:31 +1000

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+my_string = input("Введите слово: ")
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace("" , "#"))
+print(my_string.find("Я"))
+print(my_string.find("м"))
+
+
