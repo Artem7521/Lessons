0227ab25cd0f263acc6be762236289d7f23094d1
Artem <167626602+Artem7521@users.noreply.github.com>
2024-05-11 22:56:11 +1000

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ["apple", "pineapple" , "lemon"]
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:4])
+my_list[2] = "cherry"
+print(my_list)
+my_dict = {'apple': 'Яблоко', 'pineapple': 'Ананас', 'lemon': 'лимон'}
+print(my_dict)
+print(my_dict['lemon'])
+my_dict['pineapple'] = "мандарин"
+print(my_dict)
