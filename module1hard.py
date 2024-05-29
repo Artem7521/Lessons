7036c02c166bca1267a4da362c16aa84b1fdd028
Artem <167626602+Artem7521@users.noreply.github.com>
2024-05-29 15:28:03 +1000

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,11 @@
+grades = [[5,3,3,5,4,],[2,2,2,3],[4,5,5,2,],[4,4,3,] ,[5,5,5,4,5]]
+students = {'Johnny','Bilbo','Steve','Khendrik','Aaron'}
+res = sorted(students)
+print(res)
+Johny = [5,3,3,5,4]
+Bilbo = [2,2,2,3]
+Steve = [4,5,5,2]
+Khendrik =[4,4,3]
+Aaron=[5,5,5,4,5]
+average= sum(Aaron)/len(Aaron),sum(Bilbo)/len(Bilbo),sum(Johny)/len(Johny),sum(Khendrik)/len(Khendrik),sum(Steve)/len(Steve)
+print(average)
